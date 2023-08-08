4444ff6c699523030b1dfbf00fcb95213588c945
Harshika Govind <harshika0102@gmail.com>
2023-08-09 00:24:52 +0530

    added devise and authentication code for sign up and sign in

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,4 @@
 class Food < ApplicationRecord
-  belongs_to :user, foreign_key: :user_id, optional: true
+  belongs_to :user, foreign_key: :user_id
   has_many :recipe_foods, foreign_key: :food_id, dependent: :delete_all
-  validates :quantity, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
-  has_many :recipes
-  has_many :foods
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :foods, foreign_key: :user_id
+  has_many :recipes, foreign_key: :user_id
+
+  validates :name, presence: true
 end
