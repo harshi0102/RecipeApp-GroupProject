e9c5d245edaa86bf7980ad56787bc0b9291c7a6b
Harshika Govind <harshika0102@gmail.com>
2023-08-08 16:11:10 +0530

    added code for adding food form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :current_user_id
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def current_user_id
+    current_user&.id
+  end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,7 @@
 class Food < ApplicationRecord
-  belongs_to :user
-  has_many :recipe_foods
+
+  belongs_to :user, foreign_key: :user_id, optional: true
+  has_many :recipe_foods, foreign_key: :food_id, dependent: :delete_all
+  validates :quantity, presence: true
+
 end
