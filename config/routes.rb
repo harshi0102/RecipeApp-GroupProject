7348ce3d3808e244ec8601894763492da809b46d
Harshika Govind <harshika0102@gmail.com>
2023-08-10 15:59:08 +0530

    Fixed content missing issue

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   devise_for :users
-  root to: 'users#index'
+  root to: 'foods#index'
   resources :foods, only: %i[index show new create destroy]
   resources :recipes, only: %i[index show new create destroy update] do
     resources :recipe_foods, only: %i[new create destroy update]
