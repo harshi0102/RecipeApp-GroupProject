12c933e0d300ea34c481477a48d4ce1095536ec7
Harshika Govind <harshika0102@gmail.com>
2023-08-10 13:49:01 +0530

    Fixed changes

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,4 +1,10 @@
 class RecipeFood < ApplicationRecord
-  belongs_to :recipe
-  belongs_to :food
+  belongs_to :recipe, foreign_key: :recipe_id
+  belongs_to :food, foreign_key: :food_id
+
+  validates :quantity, presence: true
+
+  def calculate_value_by_quantity
+    quantity * food.price
+  end
 end
